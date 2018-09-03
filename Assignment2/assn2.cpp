b63775762b199319cbdb0099088fe25ef71a904f
Philip Kertsman <pkerts@uw.edu>
2018-09-02 17:05:03 -0700

    Yes!

--- Assignment2/assn2.cpp
+++ Assignment2/assn2.cpp
@@ -4,6 +4,8 @@
 #include <fstream>
 #include <string>
 #include <cstdint>
+#include <vector>
+#include <limits>
 
 assn2::assn2()
 {
@@ -13,6 +15,27 @@ assn2::assn2()
 assn2::~assn2()
 {
 }
+
+
+unsigned int getBit(unsigned char bit, int position)
+{
+	unsigned int mask{ 0b01111111 };
+	bit = bit << position;
+	bit = bit | mask;
+	bit = bit ^ mask;
+	bit = bit >> 7;
+	return bit;
+}
+
+
+void getByte(unsigned char byte, std::vector<unsigned int>& ints)
+{
+	for (auto i = 0; i < 8; ++i)
+	{
+		ints.emplace_back(getBit(byte, i));
+	}
+}
+
 
 
 int main()
@@ -27,39 +50,63 @@ int main()
 	// prepare file for next snippet
 	std::ofstream("test.txt", std::ios::binary) << "abcd1\nabcd2\nabcd3";
 
-	// read entire file into string
-	if (std::ifstream is{ "test.txt", std::ios::binary | std::ios::ate }) {
-		auto size = is.tellg();
-		std::string str(size, '\0'); // construct string to stream size
-		is.seekg(0);
-		if (is.read(&str[0], size))
-			std::cout << str << '\n';
-	}
+	//// read entire file into string
+	//if (std::ifstream is{ "test.txt", std::ios::binary | std::ios::ate }) {
+	//	auto size = is.tellg();
+	//	std::string str(size, '\0'); // construct string to stream size
+	//	is.seekg(0);
+	//	if (is.read(&str[0], size))
+	//		std::cout << str << '\n';
+	//}
 
 	unsigned char a = 'a';
 	unsigned char b[8] = { a };
 	std::cout << a;
 
-	int i, j;
+	std::ifstream ifs("input.txt", std::ios::binary | std::ios::ate);
 
-	unsigned int array[8] =
+	std::vector<unsigned int> ints;
+
+	if (ifs)
 	{
-		0,1,1,0,0,0,1,1,
-	};
+		int size = ifs.tellg();
+		//ifs.ignore(std::numeric_limits<std::streamsize>::max());
+		//std::streamsize size = ifs.gcount();
+		//ifs.clear();   //  Since ignore will have set eof.
+		//ifs.seekg(0, std::ios_base::beg);
+		// unsigned int* ints;
+		// ints = new unsigned int[size * 8];
+		// unsigned char* result;
+		// result = new unsigned char[size];
+		ifs.seekg(0);
+		unsigned char byte;
 
-	unsigned char result[1] = { 0 };
+		while (ifs.read(reinterpret_cast<char*>(&byte), 1))
+		{
+			getByte(byte, ints);
+		}
 
-	for (i = 0; i < 1; i++)
-	{
-		for (j = 0; j < 8; j++)
-			result[i] |= (array[i * 8 + j] << (8 - j - 1));
+		int i, j;
+
+		std::vector<unsigned char>result;
+		// unsigned char result[1] = { 0 };
+
+		for (i = 0; i < size; i++)
+		{
+			result.emplace_back(0);
+			for (j = 0; j < 8; j++)
+			{
+				result[i] |= (ints[i * 8 + j] << (8 - j - 1));
+			}
+		}
+
+		std::ofstream ofs("output.txt", std::ios::binary);
+
+		ofs.write(reinterpret_cast<char*>(&result[0]), result.size() * sizeof(unsigned char));
+		// std::cout << result[i] << std::endl;
 	}
 
-	std::ofstream ofs("output.txt", std::ios::binary);
-
-	for (i = 0; i < 1; i++)
-		ofs.write(reinterpret_cast<char*>(&result), sizeof result);
-		// std::cout << result[i] << std::endl;
+	
 
 	return 0;
 }
