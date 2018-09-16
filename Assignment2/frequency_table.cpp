242d2cf2a41c812914e340d248277bf5cd915188
Philip Kertsman <pkerts@uw.edu>
2018-09-16 05:46:15 -0700

    pop() now works. I had to put all definitions (FROM HEAP.CPP) inside of the frequency_table.cpp where the main method is.

--- Assignment2/frequency_table.cpp
+++ Assignment2/frequency_table.cpp
@@ -1,10 +1,72 @@
 #include "frequency_table.h"
-#include "Heap.cpp"
+#include "Heap.h"
 #include <fstream>
 #include <iostream>
 #include <algorithm>
 #include <string>
 #include <map>
+#include <cctype>
+#include <cassert>
+#include <memory>
+
+const int NUM_SECONDS = 5;
+
+template<typename Priority, typename Data>
+Heap<Priority, Data>::Heap()
+{
+}
+
+template<typename Priority, typename Data>
+Heap<Priority, Data>::~Heap()
+{
+}
+
+// DONE
+template<typename Priority, typename Data>
+void Heap<Priority, Data>::push(Priority priority, Data data)
+{
+	HeapNode n{ priority, data };
+	veep_.emplace_back(n);
+}
+
+// DONE
+template<typename Priority, typename Data>
+bool Heap<Priority, Data>::is_empty()
+{
+	return veep_.empty();
+}
+
+template<typename Priority, typename Data>
+void Heap<Priority, Data>::HeapSort()
+{
+	auto i = veep_.size() - 1;
+	while (i != 0)
+	{
+		if (veep_[i].priority_ > veep_[parent(i)].priority_)
+			std::swap(veep_[i], veep_[parent(i)]);
+		--i;
+	}
+}
+
+// DONE
+template<typename Priority, typename Data>
+int Heap<Priority, Data>::parent(int i)
+{
+	return ((i - 1) / 2);
+}
+
+template<typename Priority, typename Data>
+typename Heap<Priority, Data>::HeapNode Heap<Priority, Data>::pop()
+{
+	if (!is_empty())
+	{
+		auto min_value = std::min_element(veep_.begin(), veep_.end(),
+			[](HeapNode const& lhs, HeapNode const& rhs) {return lhs.priority_ < rhs.priority_; });
+		HeapNode min_return{ min_value->priority_, min_value->data_ };
+		veep_.erase(min_value);
+		return min_return;
+	}
+}
 
 //frequency_table::frequency_table(std::string filename)
 //{
@@ -174,6 +236,13 @@ auto main()->int {
 			h.push(i.second, i.first);
 		}
 	}
+
+	for (auto i = 0; i < 10; ++i)
+	{
+		auto a = h.pop();
+		std::cout << a.priority_ << std::endl;
+	}
+	
 
 	/*char ch;
 	while (std::cin.get(ch))
